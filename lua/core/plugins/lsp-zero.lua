8f1c435cdd81a7a7ba23cc36e9a1228d35ea4fa5
matt <matthewarcherr@gmail.com>
2023-08-31 20:32:08 +0100

    changed lsp-zero keybind

--- lua/core/plugins/lsp-zero.lua
+++ lua/core/plugins/lsp-zero.lua
@@ -9,4 +9,14 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- Confirm item
+    ['<C-Space>'] = cmp.mapping.confirm({select = true}),
+  }
+})
+
 lsp.setup()
